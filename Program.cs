cf33a6b4fed9f0e7b713bd43148c51a5a1d58dbc
Vitalii Kozyr <vitalii.kozyr.ua@gmail.com>
2023-12-06 00:28:23 +0200

    Query 6

--- Program.cs
+++ Program.cs
@@ -135,18 +135,17 @@ namespace Practice_Linq
         // Запит 6
         static void Query6(List<FootballGame> games)
         {
-            //Query 6: Вивести всі матчі останнього чемпіоната світу з футболу (FIFA World Cup), починаючи з чвертьфіналів (тобто останні 8 матчів).
-            //Матчі мають відображатися від фіналу до чвертьфіналів (тобто у зворотній послідовності).
+            var selectedGames = (from game in games
+                where game.Tournament == "FIFA World Cup"
+                orderby game.Date descending
+                select game).Take(8);
 
-            var selectedGames = games;   // Корегуємо запит !!!
-
-
-            // Перевірка
             Console.WriteLine("\n======================== QUERY 6 ========================");
-
-            // див. приклад як має бути виведено:
-
-
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine(
+                    $"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
         }
 
         // Запит 7
