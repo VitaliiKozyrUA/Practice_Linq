34dd40cbc5739f91736795ee4026a623c720df24
Vitalii Kozyr <vitalii.kozyr.ua@gmail.com>
2023-12-06 00:30:24 +0200

    Query 8

--- Program.cs
+++ Program.cs
@@ -169,17 +169,23 @@ namespace Practice_Linq
         // Запит 8
         static void Query8(List<FootballGame> games)
         {
-            //Query 8: Перетворити всі матчі Євро-2012 (UEFA Euro), які відбулися в Україні, на матчі з наступними властивостями:
-            // MatchYear - рік матчу, Team1 - назва приймаючої команди, Team2 - назва гостьової команди, Goals - сума всіх голів за матч
+            var selectedGames = from game in games
+                where game.Tournament == "UEFA Euro" &&
+                      game.Country == "Ukraine" &&
+                      game.Date.Year == 2012
+                select new
+                {
+                    MatchYear = game.Date.Year,
+                    Team1 = game.Home_team,
+                    Team2 = game.Away_team,
+                    Goals = game.Home_score + game.Away_score
+                };
 
-            var selectedGames = games;   // Корегуємо запит !!!
-
-            // Перевірка
             Console.WriteLine("\n======================== QUERY 8 ========================");
-
-            // див. приклад як має бути виведено:
-
-
+            foreach (var match in selectedGames)
+            {
+                Console.WriteLine($"{match.MatchYear} {match.Team1} - {match.Team2}, Goals: {match.Goals}");
+            }
         }
 
 
